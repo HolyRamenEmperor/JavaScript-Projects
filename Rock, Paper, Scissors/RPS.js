0d6feabc93c673bc4fe32a71718f219b9a02eb2e
Alan Qi <alanqi03@gmail.com>
2021-12-29 16:15:58 -0500

    Added functioning UI

--- Rock, Paper, Scissors/RPS.js
+++ Rock, Paper, Scissors/RPS.js
@@ -4,55 +4,86 @@ function computerPlay() {
         return 'Rock';
     }
     else if(num == 1) {
-        return 'Scissors';
+        return 'Paper';
     }
     else {
-        return 'Rock';
+        return 'Scissors';
     }
 }
 
 function playRound(playerSelection, computerSelection) {
-    let pS = playerSelection.toLowerCase(), cS = playerSelection.toLowerCase();
+    let pS = playerSelection.toLowerCase(), cS = computerSelection.toLowerCase();
     if(pS == 'rock') {
         if(cS == 'paper') {
-            return "You just got out-RNGed by the computer! Paper beats rock!";
+            return "(C) You just got out-RNGed by the computer! Paper beats rock!";
         }
         else if(cS = 'scissors') {
-            return "You won! Rock beats scissors!";
+            return "(P) You won! Rock beats scissors!";
         }
         else {
-            return "TIE! Rock matches rock!";
+            return "(T) TIE! Rock matches rock!";
         }
     }
     else if(pS == 'paper') {
         if(cS == 'rock') {
-            return "You won! Paper beats rock!";
+            return "(P) You won! Paper beats rock!";
         }
         else if(cS = 'scissors') {
-            return "You just got out-RNGed by the computer! Scissors beats paper!";
+            return "(C) You just got out-RNGed by the computer! Scissors beats paper!";
         }
         else {
-            return "TIE! Paper matches paper!";
+            return "(T) TIE! Paper matches paper!";
         }
     }
     else {
         if(cS == 'paper') {
-            return "You won! Scissors beats paper!";
+            return "(P) You won! Scissors beats paper!";
         }
         else if(cS = 'rock') {
-            return "You just got out-RNGed by the computer! Rock beats scissors!";
+            return "(C) You just got out-RNGed by the computer! Rock beats scissors!";
         }
         else {
-            return "TIE! Scissors matches scissors!";
+            return "(T) TIE! Scissors matches scissors!";
         }
     }
 }
-function game() {
-    for(let i = 1; i <= 5; i++) {
-        let pS = prompt("Rock, Paper, or Scissors?");
-        let cS = computerPlay();
-        let result = playRound(pS, cS);
-        console.log(result);
+let p_score = 0;
+let c_score = 0;
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach((button) => {
+
+  button.addEventListener('click', () => {
+    let cS = computerPlay();
+    let result = playRound(button.textContent, cS);
+    announcer.textContent = `${result}`;
+    if(result[1] == 'P') {
+        p_score++;
+        score_board.textContent = `Player Score: ${p_score}, Computer Score: ${c_score}`;
     }
-}
-game()
+    else if(result[1] == 'C') {
+        c_score++;
+        score_board.textContent = `Player Score: ${p_score}, Computer Score: ${c_score}`;
+    }
+    if(c_score == 5 || p_score == 5) {
+        if(p_score == 5) {
+            winner.textContent = 'You won!';
+        }
+        else {
+            winner.textContent = 'The computer won!';
+        }
+    }
+  });
+});
+const body = document.querySelector('body');
+
+const score_board = document.createElement('div');
+score_board.textContent = `Player Score: ${p_score}, Computer Score: ${c_score}`;
+
+const announcer = document.createElement('h3');
+
+const winner = document.createElement('h2');
+
+body.appendChild(score_board);
+body.appendChild(announcer);
+body.appendChild(winner);
